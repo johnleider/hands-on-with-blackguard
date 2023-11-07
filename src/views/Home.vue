9f660e151bfec4d94777aba17398a43c65fc16ab
John Leider <john@vuetifyjs.com>
2023-11-07 11:54:06 -0600

    chore(Home): remove pages boilerplate

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -64,53 +64,8 @@
           </v-btn>
         </v-col>
 
-        <v-col cols="12" class="mt-4">
-          <div class="d-flex align-center w-25 mx-auto mb-8">
-            <v-divider width="250"></v-divider>
-
-            <div class="text-h6 text-no-wrap px-2">Workshop Pages</div>
-
-            <v-divider width="250"></v-divider>
-          </div>
-
-
-          <div class="d-flex ga-4 justify-center">
-            <v-btn
-              min-width="164"
-              border
-              to="/auth/login/"
-              variant="text"
-              text="Login"
-              prepend-icon="mdi-login"
-            ></v-btn>
-
-            <v-btn
-              min-width="164"
-              border
-              to="/auth/register/"
-              variant="text"
-              text="Register"
-              prepend-icon="mdi-account-plus"
-            ></v-btn>
-
-            <v-btn
-              min-width="164"
-              border
-              to="/admin/dashboard/"
-              variant="text"
-              text="Dashboard"
-              prepend-icon="mdi-view-dashboard"
-            ></v-btn>
-
-            <v-btn
-              min-width="164"
-              border
-              to="/admin/users/"
-              variant="text"
-              text="Users"
-              prepend-icon="mdi-account-multiple"
-            ></v-btn>
-          </div>
+        <v-col cols="12">
+          <!-- Page links go here -->
         </v-col>
       </v-row>
     </v-responsive>
